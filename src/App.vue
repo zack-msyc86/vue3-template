806ed80acb1cd7934a7e3aef179fc9b4689e9c68
zack-msyc86 <masaya.okazaki.zack@gmail.com>
2022-11-07 13:43:10 +0900

    side を追加

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,38 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
-
+import Side from './components/Side.vue'
 import Sample from './components/Sample.vue'
 </script>
 
 <template>
   <Header />
-  <main>
-    <Sample />
-  </main>
+  <div class="container">
+    <main>
+      <Sample />
+    </main>
+    <aside>
+      <Side />
+    </aside>
+  </div>
   <Footer />
 </template>
 
 <style scoped lang="scss">
 
 main {
-  margin: 0 auto;
-  max-width: 1200px;
   padding: 0 32px;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  max-width: 1200px;
+}
+
+aside {
+  padding: 0 24px;
+  max-width: 20%;
 }
 </style>
---
+++ src/components/Side.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p>The Rough-skinned Newt defends itself with a deadly neurotoxin.</p>
+</template>
+
+<style scoped lang="scss">
+
+</style>
